c8cc488bd3c4f8c01fb4d1cda21314ef75f94535
nikky-001 <nikkygarg001@gmail.com>
2026-04-15 12:42:36 +0530

    Main

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import time
 from src.collector import collect_metrics
 from src.feature_engineering import process_features
-from src.health_engine import predict_health, display_output
+from src.health_engine import predict_health
 
 print("🚀 Starting AI Health Monitor...\n")
 
+current_output = {
+    "score": None,
+    "category": None
+}
+
 while True:
     try:
-        # Step 1: Collect system metrics
         metrics = collect_metrics()
-
-        # Step 2: Process features
         features = process_features(metrics)
 
-        # Step 3: Predict health score & category
         score, category = predict_health(features)
 
-        # Step 4: Display result
-        display_output(score, category)
+        # store latest output
+        current_output["score"] = score
+        current_output["category"] = category
 
-        # Run every 1 second
         time.sleep(1)
 
     except KeyboardInterrupt:
         print("\n🛑 Stopped by user")
         break
-
-    except Exception as e:
-        print(f"⚠️ Error: {e}")
-        time.sleep(1)
